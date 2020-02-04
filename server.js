f577551dedd70ffd8736fdf9a4d190312913a385
Alexander Reyes <alexandereliasr9@gmail.com>
2020-02-03 21:50:37 -0400

    comentario

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-app.set('port',80)
+app.set('port',3000)
 app.listen(app.get('port'),function(req,res){
   setInterval(function(){
     var x = new Date();
     console.log(x);
   },5000)
 });
+
+//This is the new version 
